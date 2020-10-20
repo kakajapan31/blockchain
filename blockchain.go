0dfd449d5acaf70c25f291ebcf40a75e8beee4cf
xinh.dv <xinh.dv@teko.vn>
2020-10-20 15:47:25 +0700

    lesson 3: cli and command

--- blockchain.go
+++ blockchain.go
@@ -1,15 +1,92 @@
 package main
 
+import (
+	"github.com/boltdb/bolt"
+	"log"
+)
+
+const (
+	dbFile       = "blockchain.db"
+	blocksBucket = "blocks"
+)
+
 type BlockChain struct {
-	blocks []*Block
+	tip []byte
+	db  *bolt.DB
 }
 
 func (bc *BlockChain) AddBlock(data string) {
-	prevBlock := bc.blocks[len(bc.blocks)-1]
-	newBlock := NewBlock(data, prevBlock.Hash)
-	bc.blocks = append(bc.blocks, newBlock)
+	var lastHash []byte
+
+	_ = bc.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		lastHash = b.Get([]byte("l"))
+
+		return nil
+	})
+
+	newBlock := NewBlock(data, lastHash)
+	_ = bc.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		_ = b.Put(newBlock.Hash, newBlock.Serialize())
+		_ = b.Put([]byte("l"), newBlock.Hash)
+		bc.tip = newBlock.Hash
+
+		return nil
+	})
 }
 
 func NewBlockChain() *BlockChain {
-	return &BlockChain{blocks: []*Block{NewGenesisBlock()}}
+	var tip []byte
+
+	db, err := bolt.Open(dbFile, 0600, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		if b == nil {
+			genesis := NewGenesisBlock()
+			b, _ := tx.CreateBucket([]byte(blocksBucket))
+			_ = b.Put(genesis.Hash, genesis.Serialize())
+			_ = b.Put([]byte("l"), genesis.Hash)
+			tip = genesis.Hash
+		} else {
+			tip = b.Get([]byte("l"))
+		}
+		return nil
+	})
+
+	bc := BlockChain{
+		tip: tip,
+		db:  db,
+	}
+
+	return &bc
+}
+
+type BlockChainIterator struct {
+	currentHash []byte
+	db          *bolt.DB
+}
+
+func (bc *BlockChain) Iterator() *BlockChainIterator {
+	bci := &BlockChainIterator{bc.tip, bc.db}
+	return bci
+}
+
+func (i *BlockChainIterator) Next() *Block {
+	var block *Block
+
+	_ = i.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		encodedBlock := b.Get(i.currentHash)
+		block = Deserialize(encodedBlock)
+
+		return nil
+	})
+	i.currentHash = block.PrevBlockHash
+
+	return block
 }
--- main.go
+++ main.go
@@ -1,17 +1,9 @@
 package main
 
-import "fmt"
-
 func main() {
 	bc := NewBlockChain()
+	defer bc.db.Close()
 
-	bc.AddBlock("Send 1 bitcoin to kaka")
-	bc.AddBlock("Send 2 bitcoin to kazu")
-
-	for _, block := range bc.blocks {
-		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %x\n", block.Hash)
-		fmt.Println()
-	}
+	cli := CLI{bc: bc}
+	cli.Run()
 }
